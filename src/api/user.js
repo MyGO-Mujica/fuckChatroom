10cba5c194e44821c772990f29974a2fb0dffbe6
mygo <1158270624@qq.com>
2025-05-26 14:15:40 +0800

    更新登录，注册，获取获取用户基本信息，获取历史消息的接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,14 @@
 import request from '@/utils/request'
 
+// 注册接口
+export const userRegisterService = ({ username, password, avatar }) =>
+  request.post('/reg', { username, password, avatar })
 //登录接口
-export const userLoginService = ({ username, avatar }) =>
-  request.post('/api/login', { username, avatar })
+export const userLoginService = ({ username, password }) =>
+  request.post('/login', { username, password })
 
 //获取用户的基本信息
-export const userGetInfoService = () => request.get('my/userinfo')
+export const userGetInfoService = () => request.get('/my/userinfo')
+
+//获取历史消息
+export const getHistoryMessage= ()=>request.get('/message')
