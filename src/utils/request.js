1636d59424355e90651472b1132441cca8d7aa04
mygo <1158270624@qq.com>
2025-05-28 23:17:12 +0800

    feat：新增请求拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,16 +1,21 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
-
-const baseURL = 'http://172.16.0.213:8080'
+import { useUserStore } from '@/stores'
+const baseURL = 'http://172.16.0.211:8080'
 
 const instance = axios.create({
   baseURL,
   timeout: 10000,
 })
 
+// 请求拦截器
 instance.interceptors.request.use(
   (config) => {
-    console.log('请求配置:', config) // 调试：打印请求配置
+    // TODO 2. 携带token
+    const useStore = useUserStore()
+    if (useStore.token) {
+      config.headers.Authorization = useStore.token
+    }
     return config
   },
   (err) => Promise.reject(err),
@@ -19,7 +24,7 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   (res) => {
     // 处理业务逻辑
-    if (res.data.code === 1 ) {
+    if (res.data.code === 1) {
       return res.data // 返回核心数据
     }
     // 处理业务失败
